9bfd1d066ccbc9fb531000144bcfb13100b1e5cb
moosavi.smd <moosavi.smd@arvancloud.com>
2024-09-07 05:20:17 +0330

    Terraform: put correct permisions for lambda and cloudwatch

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,10 +16,14 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_logs_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
 resource "aws_lambda_function" "test_lambda" {
   filename      = "../function.zip"
-  function_name = "lambda_function_gbfs"
+  function_name = var.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "main.fetch_and_process_data"
 
@@ -29,12 +33,32 @@ resource "aws_lambda_function" "test_lambda" {
 
   environment {
     variables = {
-      PROVIDER_1_URL="https://api.ridecheck.app/gbfs/v3/almere/vehicle_status.json"
-      PROVIDER_1_NAME="Almere"
-      PROVIDER_2_URL="https://api.ridecheck.app/gbfs/v3/amersfoort/vehicle_status.json"
-      PROVIDER_2_NAME="Amersfoort"
-      PROVIDER_3_URL="https://api.ridecheck.app/gbfs/v3/amsterdam/vehicle_status.json"
-      PROVIDER_3_NAME="Amsterdam"
+      PROVIDER_1_URL  = "https://api.ridecheck.app/gbfs/v3/almere/vehicle_status.json"
+      PROVIDER_1_NAME = "Almere"
+      PROVIDER_2_URL  = "https://api.ridecheck.app/gbfs/v3/amersfoort/vehicle_status.json"
+      PROVIDER_2_NAME = "Amersfoort"
+      PROVIDER_3_URL  = "https://api.ridecheck.app/gbfs/v3/amsterdam/vehicle_status.json"
+      PROVIDER_3_NAME = "Amsterdam"
     }
   }
+}
+
+resource "aws_cloudwatch_event_rule" "lambda_trigger" {
+  name                = "lambda-trigger-rule"
+  description         = "Triggers the Lambda function on a schedule"
+  schedule_expression = var.schedule_expression
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_trigger.name
+  target_id = var.lambda_function_name
+  arn       = aws_lambda_function.test_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_invoke" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_trigger.arn
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "lambda_function_gbfs"
+}
+
+variable "schedule_expression" {
+  description = "The rate at which to trigger the Lambda function. Example: rate(5 minutes)"
+  type        = string
+  default     = "rate(2 minutes)" # You can modify this value as needed
+}
